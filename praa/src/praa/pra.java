1a109063e7d3d82e7c154f22630857a57c3699d9
Tina <patiltina98@gmail.com>
2021-01-13 14:35:58 +0530

    nnj

---
+++ praa/src/praa/pra.java
@@ -0,0 +1,9 @@
+package praa;
+
+public class pra {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello..");
+			System.out.println("hi..");
+	}
+}
