0ea01506398b669a6c0c3ec91461cedb4fb9fcb0
Tina1998-star <77382285+Tina1998-star@users.noreply.github.com>
2021-01-13 16:33:08 +0530

    Update pra.java

--- praa/src/praa/pra.java
+++ praa/src/praa/pra.java
@@ -3,7 +3,7 @@ package praa;
 public class pra {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("hello..");
+			System.out.println("welcome..");
 			System.out.println("hi..");
 	}
 }
